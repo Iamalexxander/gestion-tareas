91307120a8e0424b2651830adee4f23c0ad4389d
Iamalexxander <yohelitoalex79@gmail.com>
2025-06-18 10:51:39 -0500

    modificación de conecction

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABSE_URL
+    connectionString: process.env.postgres_uri
 });
 
 module.exports = pool;
