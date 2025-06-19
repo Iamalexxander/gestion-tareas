713aa85ff9e7a0d2a08bf846a599ce67758e1afd
KevinJair11 <kevinchuquitarco11@gmail.com>
2025-06-18 22:57:59 -0500

    conection

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.postgres_uri
+    connectionString: "postgresql://_8842518966f103dd:_2b08d100ab8dbb22c2cac53c40ccd0@primary.database--bgf22tdmnjfd.addon.code.run:5432/_e16ec68b90a7?sslmode=require"
 
 });
 
